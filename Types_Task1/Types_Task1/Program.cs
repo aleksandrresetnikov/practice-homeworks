e8942f0161a587f5b4a2e2b76ed9afc5862a44e5
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2026-05-20 19:11:16 +0300

    feat: task 1 solution

---
+++ Types_Task1/Types_Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Types_Task1;
+
+public sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        string result = GetCompoundInterest(1000, 3, 10);
+        Console.WriteLine(result);
+    }
+
+    public static string GetCompoundInterest(double initialDeposit, int years, double interestRate)
+    {
+        StringBuilder resultBuilder = new StringBuilder();
+        double currentAmount = initialDeposit;
+
+        for (int year = 1; year <= years; year++)
+        {
+            currentAmount += currentAmount * (interestRate / 100.0);
+            
+            resultBuilder.AppendLine($"Год {year}: {currentAmount.ToString("F2", CultureInfo.InvariantCulture)} руб.");
+        }
+
+        return resultBuilder.ToString().TrimEnd();
+    }
+}
