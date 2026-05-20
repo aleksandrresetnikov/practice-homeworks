723a44e31ba981430dfd53918c87427c5986bf5b
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2026-05-20 19:11:29 +0300

    feat: task 2 solution

---
+++ Types_Task2/Types_Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Types_Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrintDiamond(7);
+    }
+
+    public static void PrintDiamond(int n)
+    {
+        if (n <= 0 || n % 2 == 0) return;
+
+        int mid = n / 2;
+
+        for (int i = 0; i < n; i++)
+        {
+            int leftSpaces = Math.Abs(mid - i);
+            Console.Write(new string(' ', leftSpaces));
+
+            Console.Write('X');
+
+            if (leftSpaces < mid)
+            {
+                int internalSpaces = n - 2 * leftSpaces - 2;
+                Console.Write(new string(' ', internalSpaces));
+                Console.Write('X');
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
